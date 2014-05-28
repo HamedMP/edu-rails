1c5edc46174321efd1adf009b926c1c28ad9e12a
Ali Ismayilov <ali@ismailov.info>
2014-05-27 21:33:51 -0300

    Add category seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+categories = ActiveSupport::JSON.decode(Rails.root.join('db', 'seeds', 'categories.json').read)
+
+categories.each do |category_attrs|
+  parent = Category.find_or_initialize_by(
+    slug: category_attrs['slug'],
+    title: category_attrs['title'],
+    order: category_attrs['order']
+  )
+  unless parent.persisted?
+    p parent if parent.save!
+  end
+
+  if category_attrs['sub-categories']
+    category_attrs['sub-categories'].each do |sub_attrs|
+      child = Category.find_or_initialize_by(
+        slug: sub_attrs['slug'],
+        title: sub_attrs['title'],
+        order: sub_attrs['order'],
+        parent: parent
+      )
+      unless child.persisted?
+        p child if child.save!
+      end
+    end
+  end
+end
