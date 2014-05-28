bbebf1a404b23e2309e75d2d5f510133624b8e87
Ali Ismayilov <ali@ismailov.info>
2014-05-28 19:55:27 -0300

    Fix broken specs

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
 describe "Categories", :type => :request do
-  describe "GET /categories" do
+  describe "GET /categories/:id" do
     it "works! (now write some real specs)" do
-      get categories_path
+      category = create :category
+      get category_path(category)
       expect(response.status).to be(200)
     end
   end
